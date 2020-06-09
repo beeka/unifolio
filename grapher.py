fb6178d05dfafac072632080b80d8e7a0d9c6ccd
Steven Blackburn <steve@beeka.org>
2020-06-10 00:50:17 +0100

    Calculate performance from a generated unit history

--- grapher.py
+++ grapher.py
@@ -103,6 +103,110 @@ class Unitiser(object):
 			return self.units
 
 
+def subtractYears(date, n=1):
+	try:
+		return date.replace(year=date.year-n)
+	except ValueError:
+		# Assume we failed on a leap-year check (e.g. 29th Feb - 1yr)
+		return date.replace(year=date.year-n, day=28)
+
+
+def getEntryBefore(entries, date):
+	if date in entries:
+		return entries[date]
+	else:
+		bestDate = None
+		for thisDate in entries.iterkeys():
+			if thisDate < date:
+				if bestDate == None or thisDate > bestDate:
+					bestDate = thisDate
+
+		if bestDate == None:
+			# No match
+			return None
+		else:
+			return entries[bestDate]
+
+# https://www.fool.com/knowledge-center/how-to-calculate-annualized-holding-period-return.aspx
+def annualisedReturn(totalReturn, years):
+	"""Calculate the annualised rate of return (i.e. compounded equivalent) for a rate acheived over a given number of years.
+	The rate is in human-readable form, i.e. 54.12 for 54.12%, as is the returned value."""
+	# (1 + totalReturn) ^ (1 / Time(years)) -1
+	if totalReturn < 0:
+		return 0.0 - annualisedReturn(abs(totalReturn), years)
+	else:
+		totalReturn = totalReturn / 100
+		arr = (1.0 + totalReturn) ** (1.0 / years) - 1
+		return arr * 100
+
+
+def ppuIncrease(entries, startDate, endDate):
+	"""Return the percentage increase from one date to another. Returns the value in human-readable form, e.g. 20.42 rather than 0.2842. Returns 0.0 on error."""
+	try:
+		startEntry = getEntryBefore(entries, startDate)
+		endEntry = getEntryBefore(entries, endDate)
+		startPrice = float(startEntry['price'])
+		endPrice = float(endEntry['price'])
+		ppuChangePercent = ((endPrice - startPrice) / startPrice) * 100
+		return ppuChangePercent
+	except:
+		return 0.0
+
+
+def determinePerformance(history):
+	"""Determine performance for the last 1,2..10 years.
+	This calculates performance backwards from the last entry in the history. This could cause the figures to be unstable as it measures the performance between different days each time the history is extended."""
+
+	performance = dict()
+
+	n = 1
+	recentDate = sorted(history.keys())[-1]
+
+	while True:
+		lastYear = subtractYears(recentDate, n)
+		if lastYear == None:
+			break;
+
+		entry = getEntryBefore(history, lastYear)
+		if entry != None:
+			ppuChangePercent = ppuIncrease(history, lastYear, recentDate)
+
+			if n > 1:
+				yearEnd = subtractYears(recentDate, n-1)
+				oneYearPerformance = ppuIncrease(history, lastYear, 		yearEnd)
+			else:
+				oneYearPerformance = ppuChangePercent
+
+			arr = annualisedReturn(ppuChangePercent, n)
+			print "Year -%s performance was %s%%. Cummulative AER of %s%% (%s%% total return)" % (n, round(oneYearPerformance, 2), round(arr, 2), round(ppuChangePercent, 2))
+			performance[n] = {
+				'duration' : n,
+				'indvidual' : oneYearPerformance,
+				'cumulative' : ppuChangePercent,
+				'effective' : arr
+			}
+			n = n + 1
+			if n > 10:
+				break
+		else:
+			# run out of whole years
+			firstDate = sorted(history.keys())[0]
+			ppuChangePercent = ppuIncrease(history, firstDate, recentDate)
+			n = float((recentDate - firstDate).days) / 365
+			arr = annualisedReturn(ppuChangePercent, n)
+			#print "All-time increase: %s%% over %s years (%s%% annualised)" % ( round(ppuChangePercent, 2), round(n, 2), "{:.2f}".format(arr))
+			print "%s%% effective annual rate of return over %s years (%s%% total return)" % (round(arr, 2), round(n, 2), round(ppuChangePercent, 2))
+			performance[n] = {
+				'duration' : n,
+				'indvidual' : None,
+				'cumulative' : ppuChangePercent,
+				'effective' : arr
+			}
+			break
+
+	return performance
+
+
 TWOPLACES = Decimal(10) ** -2
 
 def graph():
@@ -155,6 +259,10 @@ def graph():
 
 		#print date.strftime('%Y-%m-%d %H:%M:%S'), ',', value.quantize(TWOPLACES), ',', unitTracker.numberOfUnits().quantize(TWOPLACES), ',', unitTracker.pricePerUnit(value).quantize(TWOPLACES), unitTracker.invested
 
+	perf = determinePerformance(history)
+	#from pprint import pprint
+	#pprint(perf)
+	
 	return history
 
 
