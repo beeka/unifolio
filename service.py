fc10d39b45bdd677cddbba56f936f7781668824f
Steven Blackburn <steve@beeka.org>
2020-06-15 09:47:37 +0100

    Change the service.py to trigger the update / evaluate on startup and then at 23:00 each day. Also copes (ignores) issues with beautifulsoup, as it is not installed on all the development platforms, to allow testing of the service timing / output.

--- service.py
+++ service.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # Script runs the fetch and evaluation nightly
 
-def update():
-	print("Updating!")
+from datetime import *
 
-	import tracker
-	tracker.updateAll()
+def update():
+	try:
+		import tracker
+		tracker.updateAll()
+	except:
+		print("Error updating prices, using what we have")
 
 	from datetime import datetime
 	now = datetime.now()
@@ -25,12 +28,28 @@ def update():
 
 
 def runScheduler():
-	import time
+	from time import sleep
+	
+	# Run on startup, as it proves things are working (if nothing else)
+	update()
+	
+	# Set the next run time for 11pm today
+	nextTime = datetime.combine(date.today(), time(23, 0,0 , 0))
+	#nextTime = datetime.now()
+	print("Sleeping until", nextTime.strftime('%d/%m/%Y %H:%M'))
 	
 	# Simple schedule to start with... every five minutes
 	while True:
-		update()
-		time.sleep(300) # 300 seconds -> 5 minutes
+		if datetime.now() > nextTime:
+
+			print("Running portfolio update at", datetime.now().strftime('%d/%m/%Y %H:%M'))
+			update()
+
+			nextTime = nextTime + timedelta(days = 1)
+			print("Sleeping until", nextTime.strftime('%d/%m/%Y %H:%M'))
+
+		# Wait a while before checking again
+		sleep(300) # 300 seconds -> 5 minutes
 
 
 if __name__ == "__main__":
